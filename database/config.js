ab2ba93f37475d41009d41a9485f55f0077a54f8
harvey colina <harveycolina@gmail.com>
2022-10-04 01:02:41 -0400

    actualizaciones

--- database/config.js
+++ database/config.js
@@ -1,3 +1,4 @@
+
 const mongoose= require('mongoose')
 
 console.log('base de datos en linea');
@@ -6,7 +7,7 @@ console.log('base de datos en linea');
 const dbConnection = async ()=>{
 try {
     
-    await mongoose.connect(process.env.HC_CNN, {
+    mongoose.connect(process.env.HC_CNN, {
   
         
         
