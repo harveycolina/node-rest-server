10c8979674757eb22acf4381b317fb4ee98ee8fb
harvey colina <harveycolina@gmail.com>
2022-10-11 11:17:32 -0400

    manejo de error

--- database/config.js
+++ database/config.js
@@ -8,12 +8,19 @@ console.log('base de datos en linea');
     
 const dbConnection = async ()=>{
 const conect = mongoose.connect(process.env.HC_CNN)
-   
-try {
-    conect
-} catch (error) {
-    next(error)
-}
+function conect(err, req, res, next) {
+    if (res.headersSent) {
+      return next(err);
+    }
+    res.status(500);
+    res.render('error', { error: err });
+  }
+ 
+// try {
+//     conect
+// } catch (error) {
+//     next(error)
+// }
 
 
 
